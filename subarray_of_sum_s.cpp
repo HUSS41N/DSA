23a72672a2910e4777408eb803fc2e6213b35a9c
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-01 10:09:46 +0530

    Update subarray_of_sum_s.cpp

--- subarray_of_sum_s.cpp
+++ subarray_of_sum_s.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main(){
+    int n,s;
+    cin>>n>>s;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int i=0,j=0,start = -1,end = -1,sum = 0;
+    while(j<n && sum + arr[j] <= s){
+        sum += arr[j];
+        j++;
+        if(sum == s){
+            cout<<i+1<<" "<<j+1<<endl;
+            return 0;
+        }
+    }
+    while(j<n){
+        sum += arr[j];
+        while(sum>s){
+            sum -= arr[i];
+            i++;
+        }
+        if(sum==s){
+            start = i + 1;
+            end = j + 1;
+            break;
+        }
+          j++;
+    }
+  cout<<start<<" "<<end<<endl;
+}
+
 // o(n)2
 #include <iostream>
 #include <limits.h>
