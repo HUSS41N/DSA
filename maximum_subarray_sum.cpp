f7773976e1be717d51b4e941720473ba7cfce3e4
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-02 04:55:49 +0530

    o(n) solution

--- maximum_subarray_sum.cpp
+++ maximum_subarray_sum.cpp
@@ -28,5 +28,29 @@ int main(){
     }
 }
 
+// kadane's algoritham o(n)
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max_sum = INT_MIN;
+    int current_sum = 0;
+    for(int i=0;i<n;i++){
+        current_sum+=arr[i];
+        if(current_sum<0){
+            current_sum = 0;
+        }
+        max_sum = max(max_sum,current_sum);
+    }
+    cout<<max_sum;
+}
+
+
 
 
