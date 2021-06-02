12c3e0ccb52a55f87f29a4d668cd411e1648f5c6
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-02 05:58:18 +0530

    Update pair_sum.cpp

--- pair_sum.cpp
+++ pair_sum.cpp
@@ -1,3 +1,34 @@
+// o(n)
+#include <iostream>
+#include <limits.h>
+using namespace std;
+bool findpair(int arr[],int n,int k){
+   int start = 0;
+   int end = n-1;
+   while(end>start){
+       if(arr[start]+arr[end]==k){
+           cout<<start<<" "<<end;
+           return true;
+       } else if(arr[start]+arr[end]>k){
+           end--;
+       } else {
+           start++;
+       }
+   }
+    return false;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    findpair(arr,n,k);
+}
+
 // on^2
 #include <iostream>
 #include <limits.h>
