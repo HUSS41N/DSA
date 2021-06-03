5682d7ed7242eea7e02c4ece9ea024cefcf3434a
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-04 02:07:41 +0530

    Create search.cpp

---
+++ 2D Array/search.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int target;
+    cin>>target;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr[i][j]==target){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+}
