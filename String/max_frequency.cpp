b7babb244cb227b19d7e4daf2b6ad7a430a13a7e
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-05 05:00:39 +0530

    Update max_frequency.cpp

--- String/max_frequency.cpp
+++ String/max_frequency.cpp
@@ -5,6 +5,35 @@ using namespace std;
 int main()
 {
     string str = "Hello World";
+    int check[26];
+    for(int i=0;i<26;i++){
+        check[i] = 0;
+    }
+    for(int i=0;i<str.length();i++){
+        check[str[i]-'a']++;
+    }
+    int maxF = 0;
+    char maxFChar = 'a';
+    
+    for(int i=0;i<26;i++){
+        if(check[i]>maxF){
+            maxF = check[i];
+            maxFChar = i+'a';
+        }
+    }
+    cout<<maxF<<" "<<maxFChar;
+}
+
+
+
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "Hello World";
     int counter = 0;
     int mx = 0;
     char maxF = 'a';
