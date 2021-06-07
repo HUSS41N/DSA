2f79304a68224e42b1da48b858c03a8b1e636a84
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-07 08:33:03 +0530

    Update count-number-of-bits-to-be-flipped-to-convert-a-to-b.cpp

--- Bit Manipulation/count-number-of-bits-to-be-flipped-to-convert-a-to-b.cpp
+++ Bit Manipulation/count-number-of-bits-to-be-flipped-to-convert-a-to-b.cpp
@@ -6,10 +6,24 @@ int main(){
     int mask = num1^num2;
     int counter = 0;
     while(mask>0){
-        mask = mask>>1;
-        if(mask){
-            counter++;
-        }
+        mask = mask & (mask - 1);
+        counter++;
     }
     cout<<counter;
 }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int num1,num2;
+//     cin>>num1>>num2;
+//     int mask = num1^num2;
+//     int counter = 0;
+//     while(mask>0){
+//         mask = mask>>1;
+//         if(mask){
+//             counter++;
+//         }
+//     }
+//     cout<<counter;
+// }
