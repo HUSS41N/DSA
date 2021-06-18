de53786ff25201cd62b7163cbeea9971da45d5ba
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-18 13:18:24 +0530

    Update count-number-of-teams.cpp

--- Array/count-number-of-teams.cpp
+++ Array/count-number-of-teams.cpp
@@ -1,3 +1,30 @@
+class Solution {
+public:
+    int numTeams(vector<int>& arr) {
+        int counter = 0;
+        for(int i=1;i<arr.size()-1;i++){
+            int incLeft = 0; int incRight = 0; int decLeft = 0; int decRight = 0;
+            for(int j=0;j<arr.size();j++){
+                if(j<i){
+                    if(arr[j]<arr[i]){
+                        incLeft++;
+                    } else {
+                        decLeft++;
+                    }
+                }
+                if(i<j){
+                    if(arr[j]>arr[i]){
+                        incRight++;
+                    } else {
+                        decRight++;
+                    }
+                }
+            }
+            counter += (incLeft*incRight) + (decLeft*decRight);
+        }
+        return counter;
+    }
+};
 // o(n^3)
 class Solution {
 public:
