0c2dc43a83ae2b6c2f4f8ef76ae66eb3a1bb2d9b
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-19 17:50:59 +0530

    Update quickSort.cpp

--- Sorting Algorithams/quickSort.cpp
+++ Sorting Algorithams/quickSort.cpp
@@ -45,3 +45,52 @@ int main(){
         cout<<arr[i]<<" ";
     }
 }
+
+// last as pivot
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+int quickSort(int arr[], int low, int high)
+{
+    int partitionIndex;
+    if (low < high)
+    {
+        partitionIndex = partition(arr, low, high);
+        quickSort(arr, low, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, high);
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
