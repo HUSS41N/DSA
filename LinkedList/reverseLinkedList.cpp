fc4da48e05b6f3551fc3d8e85b94ab508e7063a9
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-06-22 22:52:51 +0530

    Create reverseLinkedList.cpp

---
+++ LinkedList/reverseLinkedList.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+
+        node(int value){
+            data = value;
+            next = NULL;
+        }
+};
+
+node* reverse(node* &head){
+    node* current = head;
+    node* previous = NULL;
+    node* nextPointer;
+    while(current!=NULL){
+        // store the value of next of the current 
+        nextPointer = current->next;
+        // point the pointer of the current to its previous node
+        // intital value of previous is null because after reversing list our first will be last and last points to the null
+        current->next = previous;
+        // move the previous pointer by one
+        previous= current;
+        // move the current pointer to the next value
+        current = nextPointer;
+    }
+    // after the loop our last previous will be the first element
+    return previous; 
+}
+void insertAtEnd(node* &head,int value){
+    node* n = new node(value);
+    if(head==NULL){
+        n->next = head;
+        head = n;
+        return;
+    }
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next = n;
+}
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main(){
+    node* head = NULL;
+    insertAtEnd(head,3);
+    insertAtEnd(head,2);
+    insertAtEnd(head,1);
+    display(head);
+    head = reverse(head);
+    display(head);
+}
