f5d69240ce6e16159f42311afe923bdbcca8f15c
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-07-03 22:05:41 +0530

    Create shuffle-the-array.cpp

---
+++ Array/shuffle-the-array.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/shuffle-the-array/submissions/
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
+        }
+        return res;
+    }
+};
