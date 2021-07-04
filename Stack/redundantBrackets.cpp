0167751ce8bdcae72b89c94028dbfa71565e601b
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-07-04 16:15:19 +0530

    Create redundantBrackets.cpp

---
+++ Stack/redundantBrackets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    stack<char> stk;
+    bool ans = false;
+    // approach 
+    // 1. if it is operator or opening bracket push it to stack (skip the numbers or characters)
+    // 2. if it is a closing bracket remove all the continous operators from stack and one opeinging bracket
+    // 3. if the top of the stack is opening bracket we have a redudnt bracket
+    for(auto ch : s){
+        if(ch=='+' or ch=='-' or ch == '*' or ch=='/' or ch=='('){
+            stk.push(ch);
+        }  else if (ch == ')'){
+            if(stk.top() == '('){
+                ans = true;
+            }
+            while(stk.top()=='+' or stk.top()=='-' or stk.top() == '*' or stk.top()=='/'){
+                stk.pop();
+            }
+            stk.pop();
+        }
+    }
+    cout<<ans;
+}
