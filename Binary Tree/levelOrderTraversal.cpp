0ab7d85660b5d18cba3d4cdfc64954c49275713a
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-07-09 18:33:46 +0530

    Update levelOrderTraversal.cpp

--- Binary Tree/levelOrderTraversal.cpp
+++ Binary Tree/levelOrderTraversal.cpp
@@ -52,3 +52,55 @@ int main(){
     root->right->right = new Node(7);
     levelOrderTraversal(root);
 }
+
+// using NULL to keep track of different levels
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int value){
+            data = value;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+void levelOrderTraversal(Node* root){
+    if( root == NULL ){
+        return;
+    }
+    queue<Node*> que;
+    que.push(root);
+    que.push(NULL);
+    while(!que.empty()){
+        Node* front = que.front();
+        que.pop();
+        if(front!=NULL){
+             cout<<front->data<<" ";
+            if(front->left){
+                que.push(front->left);
+            }
+            if(front->right){
+                que.push(front->right);
+            }
+        } else if(!que.empty()){
+            que.push(NULL);
+        }
+    }
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    levelOrderTraversal(root);
+}
