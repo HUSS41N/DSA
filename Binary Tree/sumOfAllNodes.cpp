4e739e2270014d583e265f8408f5c7c0fbb2b2ce
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-07-10 20:16:03 +0530

    Create sumOfAllNodes.cpp

---
+++ Binary Tree/sumOfAllNodes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+        Node(int val){
+            data = val;
+            left = NULL;
+            right = NULL;
+        }
+};
+int sumOfAllNodes(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    return sumOfAllNodes(root->left) + sumOfAllNodes(root->right) + root->data;
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout<<sumOfAllNodesStatic(root);
+}
