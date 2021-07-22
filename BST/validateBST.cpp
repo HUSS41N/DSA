9a2cb0d4347462059ab30701d8d2be307b02f5bb
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-07-23 02:06:29 +0530

    Update validateBST.cpp

--- BST/validateBST.cpp
+++ BST/validateBST.cpp
@@ -1,3 +1,39 @@
+// better solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBST(TreeNode* root,TreeNode* min,TreeNode* max){
+        if(root==NULL){
+            return true;
+        }
+        if(min != NULL && root->val <= min->val){
+            return false;
+        }
+         if(max != NULL && root->val >= max->val){
+            return false;
+        }
+        bool left = isBST(root->left,min,root);
+        bool right = isBST(root->right,root,max);
+        return left && right;
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        TreeNode* min = NULL;
+        TreeNode* max = NULL;
+        return isBST(root,min,max);
+    }
+};
+
 #include <bits/stdc++.h>
 using namespace std;
 
