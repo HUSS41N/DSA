fdc30544fe9c9e9af5188c9665d317f2b36c1882
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-07-25 14:26:18 +0530

    Create removeDuplicatesFromSortedArray.cpp

---
+++ Array/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int j = 0;
+    for(int i=0;i<n-1;i++){
+        if(arr[i] != arr[i+1]){
+            arr[j++] = arr[i];
+        }
+    }
+    arr[j++] = arr[n-1];
+    for(int i=0;i<j;i++){
+        cout<<arr[i]<<" ";
+    }
+}
