f7a328527898d25b72ddaf45f1f466433378daa8
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-08-05 18:56:22 +0530

    Create lastOccurance.cpp

---
+++ Array/lastOccurance.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int nums[n];
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int target;
+    cin>>target;
+    int start = 0;
+    int end = n - 1;
+    int ans = -1;
+    while(start<=end){
+        int mid = start + ((end - start ) / 2);
+        if(nums[mid] == target){
+            ans = mid;
+            start = mid + 1;
+        } else if (nums[mid] >  target){
+            end = mid - 1;
+        } else{
+            start = mid + 1;
+        }
+    }
+    cout<<ans;
+}
