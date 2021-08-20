eadd620fe48e73e5b05236096c6cd53277018085
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-08-20 15:56:37 +0530

    Create reverseStack.cpp

---
+++ Stack/reverseStack.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseUtil(stack<int> &s,int e){
+    if(s.empty()){
+        s.push(e);
+        return;
+    }
+    int top = s.top();
+    s.pop();
+    reverseUtil(s,e);
+    s.push(top);    
+}
+void reverse(stack<int> &s){
+    if(s.empty()){
+        return;
+    }
+    int top = s.top();
+    s.pop();
+    reverse(s);
+    reverseUtil(s,top);
+}
+void display(stack<int> s){
+    if(s.empty()){
+        return;
+    }
+    int top = s.top();
+    s.pop();
+    display(s);
+    cout<<top<<" ";
+    s.push(top);
+}
+int main(){
+    stack<int> stk;
+    stk.push(1);
+    stk.push(2);
+    stk.push(3);
+    stk.push(4);
+    stk.push(5);
+    stk.push(6);
+    display(stk);
+    reverse(stk);
+    cout<<endl;
+    display(stk);
+}
