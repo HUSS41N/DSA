4b8845cd98772382e7db3ff8fb379bd8c3280ac4
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-08-30 23:50:23 +0530

    Update substring.cpp

--- Recursion/substring.cpp
+++ Recursion/substring.cpp
@@ -1,3 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSubsets(string input,string output){
+    if(input.length() == 0){
+        cout<<output<<" ";
+        return;
+    }
+    string output1 = output;
+    string output2 = output;
+    output2.push_back(input[0]);
+    printSubsets(input.substr(1),output1);
+    printSubsets(input.substr(1),output2);
+}
+int main(){
+    string input = "ABC";
+    string output = "";
+    printSubsets(input,output);
+}
+
 #include <iostream>
 using namespace std;
 
