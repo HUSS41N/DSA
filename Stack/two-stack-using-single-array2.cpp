f4746a9b3e5d31c9370fad7193313488fb13772a
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-01 00:59:29 +0530

    Create two-stack-using-single-array2.cpp

---
+++ Stack/two-stack-using-single-array2.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 5
+
+class Stack{
+    int top1;
+    int top2;
+    public:
+        int arr[MAX];
+
+        Stack(){
+            top1 = -1;
+            top2 = MAX; 
+        }
+
+        void push1(int val){
+            if(top1 + 1 == top2){
+                cout<<"Stack Overflow\n";
+                return;
+            }
+            arr[++top1] = val;
+        }
+
+        void push2(int val){
+            if(top2 - 1 == top1){
+                cout<<"Stack Overflow\n";
+                return;
+            }
+            arr[--top2] = val;
+        }
+
+        void pop1(){
+            if(top1 == -1){
+                cout<<"Stack is Empty\n";
+                return;
+            }
+            top1--;
+        }
+
+        void pop2(){
+            if(top2 == MAX){
+                cout<<"Stack is Empty\n";
+                return;
+            }
+            top2++;
+        }
+
+        void peek1(){
+            if(top1 == -1){
+                cout<<"Stack is Empty\n";
+                return;
+            }
+            cout<<arr[top1]<<endl;
+        }
+
+        void peek2(){
+            if(top2 == MAX){
+                cout<<"Stack is Empty\n";
+                return;
+            }
+            cout<<arr[top2]<<endl;
+        }
+};
+
+int main(){
+    Stack s;
+    s.push1(1);
+    s.push1(2);
+    s.push1(3);
+    s.push1(4);
+    s.push2(11);
+    s.push2(22);
+    s.push2(33);
+    s.pop1();
+    s.peek1();
+    s.peek2();
+}
