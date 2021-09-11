37585999d52494d26c893c6fac28a6108a1bf783
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-12 00:41:40 +0530

    Update find-transition-point.cpp

--- Array/find-transition-point.cpp
+++ Array/find-transition-point.cpp
@@ -43,12 +43,36 @@ int main() {
 }// } Driver Code Ends
 
 
+// o(n)
+
 int transitionPoint(int arr[], int n) {
     // code here
     for(int i=0;i<n;i++){
         if(arr[i]==1){
             return i;
         }
+    }
+    return -1;
+}
+
+// o(logn)
+int transitionPoint(int arr[],int n){
+    int start = 0;
+    int end = n-1;
+    if(arr[0] == 1){
+        return 0;
+    }
+    while(start<=end){
+        int mid = (start + end) / 2;
+        if(arr[mid] == 1){
+            if(mid == 0 || mid > 0 and arr[mid-1] == 0){
+                 return mid;
+            }
+            end = mid -1 ;
+        }
+        else if(arr[mid]==0){
+            start = mid + 1;
+        } 
     }
     return -1;
 }
