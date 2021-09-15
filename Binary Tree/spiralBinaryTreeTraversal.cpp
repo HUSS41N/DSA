bc3d4f706262e3182a677b50fa4fd71be9cf3f5a
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-16 01:17:03 +0530

    Update spiralBinaryTreeTraversal.cpp

--- Binary Tree/spiralBinaryTreeTraversal.cpp
+++ Binary Tree/spiralBinaryTreeTraversal.cpp
@@ -62,6 +62,43 @@ void levelOrderTraversal(Node *root)
         }
     }
 }
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root==NULL){
+            return {};
+        }
+        vector<int> level;
+        queue<TreeNode*> q;
+        q.push(root);
+        level.push_back(root->val);
+        res.push_back(level);
+        q.push(NULL);
+        level = {};
+        int counter = 2;
+        while(!q.empty()){
+            TreeNode* front = q.front();
+            q.pop();
+            if(front!=NULL){
+                if(front->left){
+                    q.push(front->left);
+                    level.push_back(front->left->val);
+                }
+                if(front->right){
+                    q.push(front->right);
+                    level.push_back(front->right->val);
+                }
+            } else if(!q.empty()){
+                if(counter%2==0){
+                    reverse(level.begin(),level.end());
+                }
+                res.push_back(level);
+                q.push(NULL);
+                level = {};
+                counter++;
+            }
+        }
+        return res;
+    }
 int main()
 {
     Node *root = new Node(3);
