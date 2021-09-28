ea33e412c5fe18a4296d9ccc0e7201a1276394e2
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-09-29 00:34:29 +0530

    Update count-leaves-in-binary-tree.cpp

--- Binary Tree/count-leaves-in-binary-tree.cpp
+++ Binary Tree/count-leaves-in-binary-tree.cpp
@@ -10,3 +10,15 @@ int countLeaves(Node* root)
       return countLeaves(root->left) + countLeaves(root->right);
   }
 }
+int countLeaves(Node* root)
+{
+  if(root==NULL){
+      return 0;
+  }
+  if(root->left == NULL and root->right == NULL){
+      return 1;
+  }
+  int left = countLeaves(root->left);
+  int right = countLeaves(root->right);
+  return left + right;
+}
