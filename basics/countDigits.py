0bba6d7bd545746cf9488b3248a14154ae485dd4
Md Hussain <hussainakhtar1111@gmail.com>
2024-03-26 07:00:04 +0530

    count digits problem

--- basics/countDigits.py
+++ basics/countDigits.py
@@ -20,3 +20,17 @@ def count3(n):
     return math.floor(math.log10(n) + 1)
 
 print(count3(1990))
+
+
+# https://www.codingninjas.com/studio/problems/count-digits_8416387?leftPanelTabValue=SUBMISSION
+def countDigits(n: int) -> int:
+    count = 0
+    temp = n
+    while(n > 0):
+        lastDigit = n % 10
+        if(lastDigit != 0 and temp % lastDigit == 0):
+            count+=1
+        n = int(n/10)
+    return count
+
+countDigits(660)
