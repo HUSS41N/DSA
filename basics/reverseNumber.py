ba7b5bcf3809646b1e6af0562c6bfa7108f5267e
Md Hussain <hussainakhtar1111@gmail.com>
2024-03-26 07:27:40 +0530

    reverse integer

--- basics/reverseNumber.py
+++ basics/reverseNumber.py
@@ -1,13 +1,22 @@
 #  Given a number N reverse the number and print it.
-
+# https://leetcode.com/problems/reverse-integer/
 def reverse(n):
+    INT_MAX = 2**31 - 1
+    INT_MIN = -2**31
     ans = 0
-    while(n>0):
+    isNegative = False
+    if(0>n):
+        n = abs(n)
+        isNegative = True
+    while(n!=0):
         lastDigit = n % 10
         print(lastDigit)
         ans = ans * 10 + lastDigit
         n = int(n/10)
-    return ans
+    if(ans > INT_MAX or ans < INT_MIN):
+        return 0
+    return -1 * ans if isNegative else ans
+
 
 # print(reverse(123))
-print(reverse(1001))
+print(reverse(-123))
